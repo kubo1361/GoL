b5512612c8c3b360509d70a131481ddc6279abe1
kubo1361 <1361kubo1361@gmail.com>
2020-12-30 17:41:33 +0100

    server side threads added, communication with client estabilished

---
+++ Server/readClientInputThread.h
@@ -0,0 +1,57 @@
+//
+// Created by Eternal on 30/12/2020.
+//
+
+#ifndef SERVER_READINPUTTHREAD_H
+#define SERVER_READINPUTTHREAD_H
+
+#include <pthread.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+
+#include "Connection.h"
+
+#define BUFFER_LEN 10000
+
+struct rcitData {
+    Connection * connection;
+};
+
+void *rcitFun(void *args) {
+    rcitData *data = (rcitData *) args;
+    int n = 0;
+    char buffer[BUFFER_LEN];
+    string message;
+
+    while(true) {
+        bzero(buffer, BUFFER_LEN);
+        cout << "Waiting for clients command" << endl;
+        n = read(data->connection->getSocket(), buffer, (BUFFER_LEN - 1));
+
+
+        if (n <= 0) {
+            if(n == 0) {
+                cout << "Connection ended - deleting read thread" << endl;
+            } else {
+                perror("Error reading from socket");
+                cout << "Connection terminated - deleting read thread" << endl;
+            }
+            data->connection->setTerminateConnection(true);
+            return nullptr;
+        }
+
+        cout << "Clients command received" << endl;
+
+        message = buffer;
+
+        pthread_mutex_lock(&data->connection->getConnectionMediatorMut());
+        data->connection->addAction(message);
+        pthread_cond_signal(&data->connection->getExecuteCond());
+        pthread_mutex_unlock(&data->connection->getConnectionMediatorMut());
+
+    }
+}
+
+#undef BUFFER_LEN
+#endif //SERVER_READINPUTTHREAD_H
